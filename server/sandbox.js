a33c7e6e5533ffcdf27ef6915e41f5bb76480802
PLF77 <60972152+PLF77@users.noreply.github.com>
2022-02-17 19:45:41 +0100

    feat(shop): scrape new products

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,14 +1,22 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const montlimartbrand = require('./sources/montlimartbrand');
+const adressebrand = require('./sources/adressebrand');
+
+var fs = require('fs');
 
 async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
-    console.log(`🕵️‍♀️  browsing ${eshop} source`);
+    //console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+      const productsDedicated = await dedicatedbrand.scrape("https://www.dedicatedbrand.com/en/men/news");
+      const productsMontlimart = await montlimartbrand.scrape("https://www.montlimart.com/toute-la-collection.html");
+      const productsAdresse = await adressebrand.scrape("https://adresse.paris/630-toute-la-collection");
+      console.log('done');
+      fs.writeFileSync('dedicated.json', JSON.stringify(productsDedicated, null, 2));
+      fs.writeFileSync('montlimart.json', JSON.stringify(productsMontlimart, null, 2));
+      fs.writeFileSync('adresse.json', JSON.stringify(productsAdresse, null, 2));
 
-    console.log(products);
-    console.log('done');
     process.exit(0);
   } catch (e) {
     console.error(e);

c18a2db3ea742f1dff441c34794d0a8a5c41efe7
samkal26 <samuel.kalfon@edu.devinci.fr>
2022-02-17 13:56:07 +0100

    Features 13-15

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -13,6 +13,7 @@ const selectSort = document.querySelector('#sort-select');
 
 const filterPrice = document.querySelector('#btn-price');
 const filterRecent = document.querySelector('#btn-recent');
+const filterFavorite = document.querySelector('#btn-fav');
 
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
@@ -58,24 +59,32 @@ const fetchProducts = async (page = 1, size = 12, brand="") => {
  * @param  {Array} products
  */
 const renderProducts = products => {
-  const fragment = document.createDocumentFragment();
-  const div = document.createElement('div');
+  sectionProducts.innerHTML = `
+    <thead>
+      <tr>
+          <th>Brand</th>
+          <th>Link</th>
+          <th>Price</th>
+          <th>Favorite</th>
+      </tr>
+    </thead>
+  `
   const template = products
     .map(product => {
       return `
-      <div class="product" id=${product.uuid}>
-        <span>${product.brand}</span>
-        <a href="${product.link}" target="_blank">${product.name}</a>
-        <span>${product.price}</span>
-      </div>
+      <tr class="product" id=${product.uuid}>
+        <td>${product.brand}</td>
+        <td><a href="${product.link}" target="_blank">${product.name}</a></td>
+        <td>${product.price}</td>
+        <td><div id="fav-${product.uuid}" class="notFill" onclick="addFavorite('fav-${product.uuid}')"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+        </svg></div></td>
+      </tr>
     `;
     })
     .join('');
 
-  div.innerHTML = template;
-  fragment.appendChild(div);
-  sectionProducts.innerHTML = '<h2>Products</h2>';
-  sectionProducts.appendChild(fragment);
+  sectionProducts.innerHTML = sectionProducts.innerHTML + template;
 };
 
 /**
@@ -207,9 +216,46 @@ filterPrice.addEventListener('click', async (event) => {
   render(currentProducts, currentPagination);
 });
 
+filterFavorite.addEventListener('click', async (event) => {
+  currentProducts.sort(function (a){
+    return a.fav;
+    });
+
+  render(currentProducts, currentPagination);
+});
+
+
 document.addEventListener('DOMContentLoaded', async () => {
   const products = await fetchProducts();
 
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
+
+  $('#products').DataTable({
+    "lengthChange": false
+  });
 });
+
+function addFavorite(fav_id){
+  const heartClicked = document.querySelector('#'+fav_id);
+
+  var index = currentProducts.findIndex(x=> x.uuid === fav_id.substring(4));
+
+  if (heartClicked.getAttribute("class") == "notFill"){
+    currentProducts[index]["fav"] = true
+    heartClicked.setAttribute("class","Fill")
+    heartClicked.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+  </svg>
+  `
+  }
+  else{
+    delete currentProducts[index]["fav"] 
+    heartClicked.setAttribute("class","notFill")
+    heartClicked.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+  <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"></path>
+  </svg>`
+  }
+  
+}

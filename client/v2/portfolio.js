0d7ed0baecf2f6fbcf0c4bf499a1ca3532586a54
Theophile du Laz <theophile.dulaz@gmail.com>
2022-03-21 19:14:05 +0100

    Update portfolio.js

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -38,7 +38,7 @@ const setCurrentProducts = ({result, meta}) => {
 const fetchProducts = async (page = 1, size = 12, brand="") => {
   try {
     const response = await fetch(
-      `https://theodlz-clear-fashion-api.vercel.app?page=${page}&size=${size}&brand=${brand}`
+      `https://clear-fashion-api-theodlz.vercel.app?page=${page}&size=${size}&brand=${brand}`
     );
     const body = await response.json();
 
